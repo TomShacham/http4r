197d4d2d82fa55049dadb012c89055fd5d6713fa
tom <shachamtom@gmail.com>
2022-01-21 17:20:15 +0000

    add gpl license

--- http4r-core/src/http_message.rs
+++ http4r-core/src/http_message.rs
@@ -1,3 +1,21 @@
+/*
+    http4r is a web toolkit
+    Copyright (C) 2021 Tom Shacham
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 use std::io::{copy, Read, Write};
 use std::net::TcpStream;
 use std::str;
