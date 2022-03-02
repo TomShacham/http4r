ee0fe594191bce9d3d5476d68a9cb224a69be8a6
tom <shachamtom@gmail.com>
2022-03-02 15:09:32 +0000

    documenting test-driving an app

--- http4r-example-app/src/ok_handler.rs
+++ http4r-example-app/src/ok_handler.rs
@@ -2,10 +2,19 @@ use http4r_core::handler::Handler;
 use http4r_core::headers::Headers;
 use http4r_core::http_message::{Request, Response};
 use http4r_core::http_message::Body::BodyString;
+use http4r_core::uri::Uri;
 
 pub struct OkHandler;
+
 impl Handler for OkHandler {
-    fn handle<F>(&mut self, _req: Request, fun: F) -> () where F: FnOnce(Response) -> () + Sized {
-        fun(Response::not_found(Headers::empty(), BodyString("Not found.")));
+    fn handle<F>(&mut self, req: Request, fun: F) -> () where F: FnOnce(Response) -> () + Sized {
+        match req {
+            Request { uri: Uri { path: "/", .. }, .. } => {
+                fun(Response::ok(Headers::empty(), BodyString("Hello, world!")));
+            }
+            _ => {
+                fun(Response::not_found(Headers::empty(), BodyString("Not found.")));
+            }
+        }
     }
 }
--- http4r-example-app/tests/app_test.rs
+++ http4r-example-app/tests/app_test.rs
@@ -3,8 +3,8 @@ mod tests {
     use http4r_core::client::Client;
     use http4r_core::handler::Handler;
     use http4r_core::headers::Headers;
-    use http4r_core::http_message::Request;
-    use http4r_core::http_message::Status::NotFound;
+    use http4r_core::http_message::{body_string, Request};
+    use http4r_core::http_message::Status::{NotFound, OK};
     use http4r_core::server::Server;
     use http4r_core::uri::Uri;
     use http4r_example_app::ok_handler::OkHandler;
@@ -16,12 +16,20 @@ mod tests {
         server.start(|| { Ok(OkHandler) }, true);
 
         // request to some url we do not serve
-        let request = Request::get(Uri::parse("/not-found"), Headers::empty());
+        let not_found_req = Request::get(Uri::parse("/not-found"), Headers::empty());
 
         let mut client = Client::new("127.0.0.1", server.port, None);
         // make an http request to our server and assert response status is Not Found.
-        client.handle(request, |res| {
+        client.handle(not_found_req, |res| {
             assert_eq!(res.status, NotFound);
+            assert_eq!(body_string(res.body), "Not found.");
+        });
+
+        let ok_request = Request::get(Uri::parse("/"), Headers::empty());
+        // make a request to root that we expect to be ok
+        client.handle(ok_request, |res| {
+            assert_eq!(res.status, OK);
+            assert_eq!(body_string(res.body), "Hello, world!");
         })
     }
 }
